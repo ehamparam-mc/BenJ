0cd74b06ecdea9b7aec9afc70ac22af1ebb3917a
ehamparam-mc <ehamparam.mc@gmail.com>
2025-12-09 13:28:40 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 some backend changes and backend java
-=======
-some backend changes and front end react
->>>>>>> bebc5dbb3c6cf64d1e28a7f355cfd48834a3f995
+some front end changes and frontend react
